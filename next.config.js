816e60709c08cf6e21b87686d467161a3367b329
User <user@example.com>
2025-06-03 23:18:34 +0900

    GitHub Pages用の設定を修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
